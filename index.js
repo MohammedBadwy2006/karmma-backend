622f5e539a972af51dae1f1346892557cab56478
MohammedBadwy2006 <mohammedbdawy15@gmail.com>
2026-07-23 17:06:11 +0300

    Fix Firebase admin initialization

--- index.js
+++ index.js
@@ -7,10 +7,15 @@ const OpenAI = require("openai");
 const { initializeApp, cert } = require("firebase-admin/app");
 const { getFirestore, FieldValue } = require("firebase-admin/firestore");
 
-const serviceAccount = require("./serviceAccountKey.json");
-
 initializeApp({
-  credential: cert(serviceAccount),
+  credential: cert({
+    type: "service_account",
+    project_id: process.env.FIREBASE_PROJECT_ID,
+    private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+    private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+    client_email: process.env.FIREBASE_CLIENT_EMAIL,
+    client_id: process.env.FIREBASE_CLIENT_ID,
+  }),
 });
 
 const db = getFirestore();
